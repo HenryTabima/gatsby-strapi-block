e35e9912074ab8754841eafc19158da9c9ec3be5
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-10 15:52:36 -0500

    conection added to strapi cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,58 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
+import Reactmarkdown from 'react-markdown'
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <ul>
+      {data.allStrapiArticle.edges.map(document => (
+        <li key={document.node.id}>
+          <h2>
+              <Link to={`/${document.node.id}`}>{document.node.title}</Link>
+          </h2>
+          <Img fixed={document.node.image.childImageSharp.fixed} />
+          <Reactmarkdown
+            source={document.node.content.substring(0, 500).concat("...")}
+            transformImageUri={uri =>
+              uri.startsWith("http")
+                ? uri
+                : `${process.env.IMAGE_BASE_URL}${uri}`
+            }
+            className="indexArticle"
+            escapeHtml={false}
+          />
+          <Link to={`/${document.node.id}`}>Leer más</Link>
+        </li>
+      ))}
+    </ul>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query indexQuery {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          image {
+            childImageSharp {
+              fixed(width: 200, height: 125) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          title
+          content
+        }
+      }
+    }
+  }
+`
---
+++ src/templates/author.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import Reactmarkdown from "react-markdown"
+
+const UserTemplate = ({ data }) => (
+  <Layout>
+    <h1>{data.strapiUser.username}</h1>
+    <ul>
+      {data.strapiUser.articles.map(article => (
+        <li key={article.id}>
+          <h2>
+            <Link to={`/Article_${article.id}`}>{article.title}</Link>
+          </h2>
+
+          <Reactmarkdown
+            source={article.content.substring(0, 500).concat("...")}
+            transformImageUri={uri =>
+              uri.startsWith("http")
+                ? uri
+                : `${process.env.IMAGE_BASE_URL}${uri}`
+            }
+            className="indexArticle"
+            escapeHtml={false}
+          />
+          <Link to={`/Article_${article.id}`}>Leer más</Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+export default UserTemplate
+
+export const query = graphql`
+  query UserTemplate($id: String!) {
+    strapiUser(id: { eq: $id }) {
+      id
+      username
+      articles {
+        id
+        title
+        content
+      }
+    }
+  }
+`
